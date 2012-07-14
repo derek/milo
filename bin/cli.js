35b80eca4e7e3968d9e8ab27ceb104ee534d0efb
Derek Gathright <drgath@gmail.com>
2012-07-14 14:00:25 -0700

    Moved plugins to /lib/ and did more refactoring

--- bin/cli.js
+++ bin/cli.js
@@ -1,132 +1,29 @@
 #!/usr/bin/env node
-/*jslint sloppy:true*/
+/*jslint nomen:true sloppy:true white:true node:true*/
 
-var yql  = require('yql'),
+var yql = require('yql'),
     fs = require('fs'),
-    configPublic = require(process.env.NODE_PATH + 'milo/config.public')
-    configPrivate = require(process.env.NODE_PATH + 'milo/config.private')
+    configPublic = require('milo/config.public'),
+    configPrivate = require('milo/config.private'),
     argv = require('optimist').argv,
-    log = console.log,
-    config = {};
+    config = {},
+    i;
 
-for (var i in configPublic) {
-    config[i] = configPublic[i]
-}
-for (var i in configPrivate) {
-    config[i] = configPrivate[i]
-}
-
-function main() {
-    var arg = argv._[0];
-    switch(arg) {
-        case "tweets":
-            getTweets();
-            break;
-
-        case "modules":
-            listModules();
-            break;
-        
-        case "coverage":
-            coverage();                
-            break;
-
-        case "gist":
-            gist(argv._[1]);
-            break;
-        
-        default:
-            print_info();
-            break;
+for (i in configPublic) {
+    if (configPublic.hasOwnProperty(i)) {
+        config[i] = configPublic[i];   
     }
 }
 
-main();
-
-
-function print_info () {
-    var fs = require('fs'),
-        file = process.env.NODE_PATH + 'milo/assets/milo.txt'
-
-    fs.readFile(file, 'ascii', function(err,data){
-      if(err) {
-        console.error("Could not open file: %s", err);
-        process.exit(1);
-      }
-
-      console.log(data);
-    });
-
+for (i in configPrivate) {
+    if (configPrivate.hasOwnProperty(i)) {
+        config[i] = configPrivate[i];   
+    }
 }
 
-function listModules () {
-    var path = config.yuiPath,
-        dir = path + '/build'
-
-    fs.readdir(dir, function(err, list) {
-        if (err) throw err;
-        list.forEach(function (module) {
-            log(module);
-        })
-    });
-}
-
-function coverage () {
-    var child_process = require('child_process'),
-        spawn = require('child_process').spawn,
-        coverage = spawn('yui-coverage');
-        
-    coverage.stdout.on('data', function (data) {
-      console.log('stdout: ' + data);
-    });
-
-    coverage.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
-    });
-
-    coverage.on('exit', function (code) {
-      console.log('child process exited with code ' + code);
-    });
-}
-
-function getTweets () {
-
-    new yql.exec("select * from twitter.user.timeline where (id = @id)", function(response) {
-
-        if (response.error) {
-            console.log(require('util').inspect(error));
-            console.log("Example #2... Error: " + response.error.description);
-        } 
-        else {
-            var tweets = response.query.results.statuses.status;
-
-            tweets.forEach(function (tweet) {
-                console.log(tweet.created_at + ": " + tweet.text);
-            })
-        }
-
-    }, {id:"yuilibrary"}, {ssl:true});
-}
-
-function gist (file) {
-    /*
-    To get a token:
-    curl https://api.github.com/authorizations -d '{"scopes": ["gist"],"note": "admin script"}' --user username:password
-    */
-    var Gister = require('gister'),
-        path = require('path'),
-        body = fs.readFileSync(file, 'utf-8'),
-        filename = path.basename(file),
-        data = {},
-        gist = new Gister({
-            token: config.github.token
-        });
-
-    gist.on('created', function(response) {
-        log('URL: ' + response.files[filename].raw_url);
-    })
-
-    data[filename] = body;
-
-    gist.create(data);
-}
+(function () {
+    var plugin = argv._.shift() || 'help',
+        args = argv._;
+    
+    require('milo/lib/' + plugin).apply(this, args);
+})();

d5cc3b409dc0d945a74d9b123e0be52bcb865cb8
Derek Gathright <drgath@gmail.com>
2012-07-16 23:30:40 -0700

    Cleanup

--- utils.js
+++ utils.js
@@ -1,30 +1,32 @@
-var getConfig = function () {
-
-	var configPublic = require('milo/config.public'),
-		configPrivate = require('milo/config.private'),
-		config = {},
-		i;
-
-	for (i in configPublic) {
-	    if (configPublic.hasOwnProperty(i)) {
-	        config[i] = configPublic[i];   
-	    }
-	}
-
-	for (i in configPrivate) {
-	    if (configPrivate.hasOwnProperty(i)) {
-	        config[i] = configPrivate[i];   
-	    }
-	}
-
-	return config;
-}
-
-var getAsset = function (file) {
-	return require('fs').readFileSync(process.env.NODE_PATH + 'milo/assets/' + file, 'utf-8');
-}
-
 module.exports = {
-	getConfig: getConfig,
-	getAsset: getAsset
+
+	getConfig: function () {
+
+		var configPublic = require('milo/config.public'),
+			configPrivate = require('milo/config.private'),
+			config = {},
+			key;
+
+		for (key in configPublic) {
+		    if (configPublic.hasOwnProperty(key)) {
+		        config[key] = configPublic[key];
+		    }
+		}
+
+		for (key in configPrivate) {
+		    if (configPrivate.hasOwnProperty(key)) {
+		        config[key] = configPrivate[key];
+		    }
+		}
+
+		return config;
+		
+	},
+
+	getAsset: function (file) {
+
+		return require('fs').readFileSync(process.env.NODE_PATH + 'milo/assets/' + file, 'utf-8');
+
+	}
+
 };

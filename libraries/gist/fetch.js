255223989b405be6d4056e74beb8c3c2c21b3c01
Derek Gathright <drgath@gmail.com>
2012-07-24 12:01:05 -0700

    Moved gist utils to their own library

---
+++ libraries/gist/fetch.js
@@ -0,0 +1,29 @@
+module.exports = function (id, dir) {
+    var gister = require('./utils').getGister(this.config),
+        fs = require('fs'),
+        exec = require('child_process').exec;
+
+    dir = dir || id.toString();
+
+    if (!fs.existsSync(dir)) {
+
+        console.log('');
+        console.log('Cloning...');
+
+        gister.on('gist', function(gist) {
+            fs.mkdirSync(dir);
+            exec('git clone ' + gist.git_push_url + ' ' + dir).on('exit', function (code) {
+                console.log('Done');
+                console.log('');
+                console.log('./' + dir + '/');
+                fs.readdirSync(dir).forEach(function(file){
+                    console.log('\t' + file);
+                });
+            });
+        }).get(id);
+        
+    }
+    else {
+        console.log("Error: Directory already exists");
+    }
+};
